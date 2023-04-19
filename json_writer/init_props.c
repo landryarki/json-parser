f537ce06b3ccae5cc92f0e8553ae5583ec518059
landryarki <landry.gigant@epitech.eu>
2023-04-19 22:54:47 +0200

    :bug: fix a crash occurd when appening a props to an object

--- json_writer/init_props.c
+++ json_writer/init_props.c
@@ -7,15 +7,24 @@
 
 #include "my_json_writer_back.h"
 #include "utilities.h"
+#include <stdlib.h>
 
 json_props_t *json_init_object(char *key)
 {
     json_props_t *json = json_create_props(my_strdup(key), JSON_OBJECT, NULL);
+    json->data = malloc(sizeof(json_props_t *));
+    if (json->data == NULL)
+        return NULL;
+    ((json_props_t **)json->data)[0] = NULL;
     return json;
 }
 
 json_props_t *json_init_array(char *key)
 {
     json_props_t *json = json_create_props(my_strdup(key), JSON_ARRAY, NULL);
+    json->data = malloc(sizeof(json_props_t *));
+    if (json->data == NULL)
+        return NULL;
+    ((json_props_t **)json->data)[0] = NULL;
     return json;
 }
